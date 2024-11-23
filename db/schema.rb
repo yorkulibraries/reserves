6c3e0f4ddf2aeb3d77c87e04999372bbe2f129a3
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-23 10:28:06 -0500

    datetime precision should be db default

--- db/schema.rb
+++ db/schema.rb
@@ -18,15 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.string "status"
     t.integer "cancelled_by_id"
     t.text "cancellation_reason"
-    t.datetime "cancelled_at", precision: 6
+    t.datetime "cancelled_at"
     t.integer "acquired_by_id"
-    t.datetime "acquired_at", precision: 6
+    t.datetime "acquired_at"
     t.text "acquisition_notes"
     t.text "acquisition_source_type"
     t.text "acquisition_source_name"
     t.integer "list_id"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "location_id"
   end
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.integer "version", default: 0
     t.text "comment"
     t.string "remote_address"
-    t.datetime "created_at", precision: 6
+    t.datetime "created_at"
     t.string "request_uuid"
     t.index ["associated_id", "associated_type"], name: "associated_index"
     t.index ["auditable_id", "auditable_type"], name: "auditable_index"
@@ -58,8 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.integer "student_count"
     t.string "instructor"
     t.integer "created_by_id"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "code_year"
     t.string "code_faculty"
     t.string "code_subject"
@@ -71,15 +71,15 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
   create_table "courses_faculties", charset: "latin1", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "courses_subjects", charset: "latin1", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", id: :integer, charset: "utf8mb3", force: :cascade do |t|
@@ -96,8 +96,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.string "edition"
     t.string "loan_period"
     t.boolean "provided_by_requestor", default: false
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "item_type"
     t.string "copyright_options"
     t.text "other_copyright_options"
@@ -115,8 +115,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
 
   create_table "loan_periods", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.string "duration"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "locations", id: :integer, charset: "utf8mb3", force: :cascade do |t|
@@ -125,8 +125,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.string "contact_phone"
     t.text "address"
     t.boolean "is_deleted", default: false
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "disallowed_item_types"
     t.boolean "setting_bcc_request_status_change", default: false
     t.string "ils_location_name"
@@ -146,12 +146,12 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.date "reserve_end_date"
     t.string "status"
     t.boolean "removed_from_reserves", default: false
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "requester_email"
     t.integer "rollover_parent_id"
-    t.datetime "rolledover_at", precision: 6
-    t.datetime "removed_at", precision: 6
+    t.datetime "rolledover_at"
+    t.datetime "removed_at"
     t.integer "removed_by_id"
   end
 
@@ -160,8 +160,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.text "value"
     t.integer "thing_id"
     t.string "thing_type", limit: 30
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
   end
 
@@ -179,9 +179,9 @@ ActiveRecord::Schema[7.0].define(version: 2021_08_20_131142) do
     t.boolean "admin", default: false
     t.integer "location_id"
     t.integer "created_by_id"
-    t.datetime "last_login", precision: 6
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "last_login"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
