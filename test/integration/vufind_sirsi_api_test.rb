89dfddb18ba11e49c06cedd4f9ee595047d93213
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-23 11:28:19 -0500

    remove unused vufind test

--- test/integration/vufind_sirsi_api_test.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class VufindSirsiApiTest < ActionDispatch::IntegrationTest
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,63 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
+# some of the comments are in UTF-8
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
+require 'factory_girl_rails'
+require 'minitest/unit'
+require 'database_cleaner/active_record'
+
+# Configure shoulda-matchers to use Minitest
 require 'shoulda/matchers'
 
-class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
-  include ActionMailer::TestHelper
-  include ActiveJob::TestHelper
+DatabaseCleaner.url_allowlist = [
+  %r{.*test.*}
+]
+DatabaseCleaner.strategy = :truncation
+
+include ActionDispatch::TestProcess
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
+end
+
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
+
+    # fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+    include FactoryGirl::Syntax::Methods
+    include Warden::Test::Helpers
+    include ActionMailer::TestHelper
+    include ActiveJob::TestHelper
+
+    Warden.test_mode!
+
+    setup do
+      DatabaseCleaner.start
+    end
+
+    teardown do
+      DatabaseCleaner.clean
+    end
+  end
 end
 
 class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   include ActionDispatch::TestProcess
+  include ActionMailer::TestHelper
+
   include Warden::Test::Helpers
-  Devise::Test::IntegrationHelpers
   Warden.test_mode!
 
   def log_user_in(user)
@@ -28,10 +71,4 @@ class ActionDispatch::IntegrationTest
   end
 
   def assert_template(which); end
-end
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :minitest
-    with.library :rails
-  end
 end
