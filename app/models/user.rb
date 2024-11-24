651b42025e2a28ea7741b1703c50fedda9331e90
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-24 08:37:56 -0500

    add devise for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :trackable, :lockable
+  
   ## searchable Concern
   include Searchable
-  devise :registerable
 
   ## CONSTANTS
   FACULTY = 'FACULTY'
