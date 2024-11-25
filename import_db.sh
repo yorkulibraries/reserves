93943e7b3e0836e845033469ce4db29f9f5b2d9c
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-24 20:17:21 -0500

    move import_db.sh to bin and update compose

--- import_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dump=$1
-
-[ -f "$dump" ] && rails db:reset &&  sed '/DROP DATABASE \|CREATE DATABASE \|USE /d' "$dump" | sed 's/ datetime / datetime(6) /' | rails db -p
-
-rails db:migrate
