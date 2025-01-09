1991cd93a891fef403ba63885c0b2b5ef5c422f6
Tuan Nguyen <amtuannguyen@gmail.com>
2025-01-09 18:26:37 -0500

    drop tables instead of database in import script

--- bin/import_db.sh
+++ bin/import_db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 dump=$1
 
-[ -f "$dump" ] && rails db:drop && rails db:create && zcat -f "$dump" | sed '/DROP DATABASE \|CREATE DATABASE \|USE `/d'  | sed 's/ datetime / datetime(6) /'  > import.sql 
+[ -f "$dump" ] &&  zcat -f "$dump" | grep 'DROP TABLE ' > drop.sql 
+[ -f "$dump" ] &&  zcat -f "$dump" | sed '/DROP DATABASE \|CREATE DATABASE \|USE `/d'  | sed 's/ datetime / datetime(6) /'  > import.sql 
 
+cat drop.sql | rails db -p
 cat import.sql | rails db -p
 
 rails db:migrate
