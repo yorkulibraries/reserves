1fbb051d2ecd4956fcd0bbe41aa79239c8dacdf3
Tuan Nguyen <amtuannguyen@gmail.com>
2025-01-10 10:47:55 -0500

    use show tables to list all tables in db to drop when importing db

--- bin/import_db.sh
+++ bin/import_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 dump=$1
 
-[ -f "$dump" ] &&  zcat -f "$dump" | grep 'DROP TABLE ' > drop.sql 
+echo 'show tables;' | rails db -p | grep -v "Tables_in_" | xargs -i -n 1 echo "DROP TABLE IF EXISTS \`{}\`;" > drop.sql 
+
 [ -f "$dump" ] &&  zcat -f "$dump" | sed '/DROP DATABASE \|CREATE DATABASE \|USE `/d'  | sed 's/ datetime / datetime(6) /'  > import.sql 
 
 cat drop.sql | rails db -p
